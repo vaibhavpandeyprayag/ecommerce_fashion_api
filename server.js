69ff0fbda88f2fdcf0393f36e474ceb182435c4b
Vaibhav <vaibhavpandeyprayag@gmail.com>
2025-07-11 03:54:03 +0530

    added port and root route

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ app.use(express.json());
 app.use(cors(["*"]));
 app.use("/api", router);
 
-app.listen("5000", "localhost", () => {
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
   console.log("Server is running on port 5000");
+});
+
+app.get("/", (req, res) => {
+  res.send("Congratulations! Server is running");
 });
